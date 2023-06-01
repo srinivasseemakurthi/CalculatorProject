a5d2c36f01538ace9c5b7146ed9f01d8e00a041f
srinivasseemakurthi <srinivas.seemakurthi@dymensions.io>
2023-06-01 13:43:29 +0530

    appended semicolon
    Semicolon is missing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+let num1 = 8;
+let num2 = 2;
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
@@ -14,21 +14,21 @@ let sumEl = document.getElementById("sum-el")
 
 function add() {
     let result = num1 + num2
-    sumEl.textContent = "Sum: " + result
+    sumEl.textContent = "Sum: " + result;
 }
 
 function subtract() {
     let result = num1 - num2
-    sumEl.textContent = "Sum: " + result
+    sumEl.textContent = "Sum: " + result;
 }
 
 function divide() {
     let result = num1 / num2
-    sumEl.textContent = "Sum: " + result
+    sumEl.textContent = "Sum: " + result;
 }
 
 function multiply() {
     let result = num1 * num2
-    sumEl.textContent = "Sum: " + result
+    sumEl.textContent = "Sum: " + result;
 }
 
